ec5d1363ccf5aabc473b9102b80687fdefa4a87a
odavidmedina <odavidmedina@gmail.com>
2024-07-17 21:47:39 -0300

    2 commit

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ function solicitarDatos() {
     const nombre = prompt("Ingresa tu nombre:");
     if (nombre === null) {
         alert("Operación cancelada. No se ingresaron datos.");
-        
+        return;
     } else if (nombre.trim() === "") {
         alert("El nombre no puede estar vacío. Por favor, intenta de nuevo.");
+        return;
     }
     const apellido = prompt("Ingresa tu apellido:");
     if (apellido === null) {
         alert("Operación cancelada. No se ingresaron datos.");
+        return;
     } else if (apellido.trim() === "") {
         alert("El apellido no puede estar vacío. Por favor, intenta de nuevo.");
+        return;
     }
     const edad = prompt("Ingresa tu edad:");
-    if (apellido === null) {
+    if (edad === null) {
         alert("Operación cancelada. No se ingresaron datos.");
-    } else if (apellido.trim() === "") {
+        return;
+    } else if (edad.trim() === "") {
         alert("La edad no puede estar vacío. Por favor, intenta de nuevo.");
+        return;
     }
     const confirmar = confirm(`¿Es correcta la información?\nNombre: ${nombre}\nApellido: ${apellido}\nEdad: ${edad}`);
     if (confirmar) {
@@ -33,5 +38,6 @@ function solicitarDatos() {
         console.log("Datos actuales:", datos);
     } else {
         alert("Operación cancelada. No se ingresaron datos.");
+        return;
     }
 }
